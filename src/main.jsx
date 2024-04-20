201e62be3c711b8ab310ca5cf1af273cdaebd835
Ariel <thepiratemermaid4@gmail.com>
2024-04-20 16:37:34 -0600

    reorder imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
 import { ChakraProvider } from '@chakra-ui/react';
+
+import App from './App.jsx';
 import theme from './theme.js';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
