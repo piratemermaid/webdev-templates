b59573d523c2188e27fd8c518c7c9d5da81a44da
Ariel <thepiratemermaid4@gmail.com>
2024-04-20 16:36:52 -0600

    add dark mode toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { Button, useColorMode } from '@chakra-ui/react';
+
 import './App.css';
 
 function App() {
+  const { toggleColorMode } = useColorMode();
+
   return (
     <>
       <h1>Hello There</h1>
+      <Button onClick={toggleColorMode}>Toggle Dark Mode</Button>
     </>
   );
 }
